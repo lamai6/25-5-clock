4b528edb878fe4921b18e2d8e0549f2276e2a116
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-04-25 18:52:27 +0200

    test(tdd): add test for user story #2

--- src/components/Pomodoro/Pomodoro.test.jsx
+++ src/components/Pomodoro/Pomodoro.test.jsx
@@ -3,11 +3,19 @@ import '@testing-library/jest-dom';
 import Pomodoro from './Pomodoro';
 
 describe('Product backlog test suite', () => {
-  it('should display an element with "break-label" id containing "Break Length" text (US#1)', () => {
+  it('should display an element with "break-label" id containing "Break Time" text (US#1)', () => {
     const { container } = render(<Pomodoro />);
     const breakLabel = container.querySelector('div[id=break-label]');
 
     expect(breakLabel).toBeInTheDocument();
-    expect(breakLabel).toHaveTextContent('Break Length');
+    expect(breakLabel).toHaveTextContent('Break Time');
+  });
+
+  it('should display an element with "session-label" id containing "Session Time" text (US#2)', () => {
+    const { container } = render(<Pomodoro />);
+    const sessionLabel = container.querySelector('div[id=session-label]');
+
+    expect(sessionLabel).toBeInTheDocument();
+    expect(sessionLabel).toHaveTextContent('Session Time');
   });
 });
