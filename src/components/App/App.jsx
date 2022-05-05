e6c29ac61ab224594935b31abf6cf7e38b29c2e3
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-05-05 20:20:20 +0200

    feat: add fcc test suite

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    localStorage.setItem('project_selector', '25-5-clock');
+    localStorage.setItem('fCC_25-5-clock_hide', 'true');
   }
 
   render() {
