770dd65b51fb5b8a56e43467d88cba79343f0014
lamai6 <26470211+lamai6@users.noreply.github.com>
2022-05-05 20:47:38 +0200

    fix(audio): fix dom exception when pause() is called during play() execution

--- src/components/Audio/Audio.jsx
+++ src/components/Audio/Audio.jsx
@@ -8,15 +8,20 @@ function Audio({ activeTime, shouldRunning, shouldResetTimer }) {
 
   usePropsUpdate(async () => {
     if (shouldRunning) {
-      const { current: audio } = audioRef;
-      audio.volume = 0.01;
-      audio.currentTime = 0;
-      await audio.play();
+      try {
+        const { current: audio } = audioRef;
+        audio.volume = 0.2;
+        audio.currentTime = 0;
+        await audio.play();
+      } catch (err) {
+        // fixes `play() request was interrupted by a call to pause()` (https://goo.gl/LdLk22)
+      }
     }
   }, [activeTime]);
 
   usePropsUpdate(async () => {
     const { current: audio } = audioRef;
+    audio.currentTime = 0;
     await audio.pause();
   }, [shouldResetTimer]);
 
